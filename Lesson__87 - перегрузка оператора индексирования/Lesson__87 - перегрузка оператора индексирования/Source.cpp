e18bd77b6ff4fbf5f014312d84fb301a2f41d4bb
pashacyborg <pashacyborg2@gmail.com>
2022-04-04 20:02:51 +0300

    перегрузка оператора индексирования

---
+++ Lesson__87 - перегрузка оператора индексирования/Lesson__87 - перегрузка оператора индексирования/Source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+
+/*Перегрузка оператора индексирования используется в таком контейнере для данных, как вектор в библиотеке STL.
+Ссылка создает связь между функциями. Если тип возвращаемого значения ссылка на объект, то мы можем из внешней
+функции изменить поля класса по ссылке
+*/
+
+class TestClass
+{
+public:
+	int& operator [] (int index)
+	{
+		return mas[index];
+	}
+
+private:
+	int mas[10]{ 228,1488,42,4,21,8,7,23,88,9 };
+};
+
+int main()
+{
+	TestClass object1;
+	cout << object1[1] << endl;
+	cout << object1.operator[](4) << endl;		//Результат будет одинаковым
+
+	return 0;
+}
